b3382dbaae9576af61a51500fcf8e18fa43c9971
Nifras365 <nifrasmjm56@outlook.com>
2024-07-11 15:14:36 +0530

    Initial commit

---
+++ olsbackend/src/main/java/laptop_store/olsbackend/OlsbackendApplication.java
@@ -0,0 +1,13 @@
+package laptop_store.olsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlsbackendApplication.class, args);
+	}
+
+}
---
+++ olsbackend/src/test/java/laptop_store/olsbackend/OlsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package laptop_store.olsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
