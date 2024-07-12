1cff5a9062a812d7d6bda30cd64530625d7eeb3f
Nifras365 <nifrasmjm56@outlook.com>
2024-07-12 15:58:43 +0530

    navbar

--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import NavbarBL from "../components/NavbarBL";
 
 const Welcome = () => {
     return(
     <div>
         <h1>Laptop Store</h1>
+        <NavbarBL/>
     </div>
     )
 }
