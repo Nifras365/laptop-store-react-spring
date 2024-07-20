93474ed0494da5cd783acc98d8d9c3d6b3470ed2
Nifras365 <nifrasmjm56@outlook.com>
2024-07-20 19:53:53 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Welcome from './pages/welcome';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Welcome/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/NavbarBL.jsx
+++ frontend/src/components/NavbarBL.jsx
@@ -13,7 +13,7 @@ const NavbarBL = () => {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="login">Login</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
