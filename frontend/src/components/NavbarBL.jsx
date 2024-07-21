1eb259bc35097a54b4a83537d257ca23a63c265b
Nifras365 <nifrasmjm56@outlook.com>
2024-07-21 22:24:54 +0530

    Update NavbarBL.jsx

--- frontend/src/components/NavbarBL.jsx
+++ frontend/src/components/NavbarBL.jsx
@@ -9,7 +9,7 @@ const NavbarBL = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <NavbarBrand href="#home">MyBrand</NavbarBrand>
+        <NavbarBrand href="#home">MyLapStore</NavbarBrand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -29,7 +29,7 @@ const NavbarBL = () => {
             <NavDropdown className='dropdown-title' title={<IoPersonOutline size={25}/>} id="basic-nav-dropdown">
               <NavDropdown.Item href="/login">Login</NavDropdown.Item>
               <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="/cart">Cart</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
