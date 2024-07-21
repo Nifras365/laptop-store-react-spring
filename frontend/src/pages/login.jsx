73f8205f8122aab7aba2e40fe891bc69e5a6b5fd
Nifras365 <nifrasmjm56@outlook.com>
2024-07-21 20:43:51 +0530

    css-login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import '../pagescss/login.css'
 
 const Login = () =>{
     return(
     <div>
         <h1>Login Page</h1>
-            <Container>
-                <form>
+            <Container className="login-container">
+                <form className="login-form">
                     <div>
                         <label>Email</label>
                         <input/>
