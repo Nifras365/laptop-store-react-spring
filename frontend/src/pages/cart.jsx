ad47ddc07aa8f573761ffb33cf2b0e285f8986ba
Nifras365 <nifrasmjm56@outlook.com>
2024-07-23 00:37:06 +0530

    Update cart.jsx

--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -4,6 +4,7 @@ const Cart = () => {
     return(
         <div>
             <h1>Under Construction</h1>
+            <h2>Some laptop should be added here this is a cart...</h2>
         </div>
     );
 }
