5b3b761e45b8ee4b43d7074aea4fdc6ed487db7f
Nifras365 <nifrasmjm56@outlook.com>
2024-07-22 21:07:12 +0530

    Update LaptopCard.jsx

--- frontend/src/components/LaptopCard.jsx
+++ frontend/src/components/LaptopCard.jsx
@@ -1,3 +1,27 @@
 import React from "react";
 import './LaptopCard.css';
+import { Card, Button, Row, Col } from "react-bootstrap";
 
+const LaptopCard = ({ image, price, description }) => {
+    return (
+        <Card className="laptop-card">
+            <Card.Img variant="top" src={image} className="laptop-card-img" />
+            <Card.Body>
+                <Card.Title>${price}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Row>
+                    <Col>
+                        <Button variant="primary" className="add-to-cart-btn">Add to Cart</Button>
+                    </Col>
+                    <Col>
+                        <Button variant="success" className="buy-now-btn">Buy Now</Button>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default LaptopCard;
