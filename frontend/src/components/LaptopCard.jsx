771aff7b97af4cdd0a8182fc2da7633e853de602
Nifras365 <nifrasmjm56@outlook.com>
2024-07-22 21:47:39 +0530

    cart-icon

--- frontend/src/components/LaptopCard.jsx
+++ frontend/src/components/LaptopCard.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import './LaptopCard.css';
 import { Card, Button, Row, Col } from "react-bootstrap";
+import { IoCart } from "react-icons/io5";
 
 const LaptopCard = ({ image, price, description }) => {
     return (
         <Card className="laptop-card">
             <Card.Img variant="top" src={image} className="laptop-card-img" />
             <Card.Body>
-                <Card.Title>${price}</Card.Title>
+                <Card.Title>Price: ${price}</Card.Title>
                 <Card.Text>
                     {description}
                 </Card.Text>
                 <Row>
                     <Col>
-                        <Button variant="primary" className="add-to-cart-btn">Add to Cart</Button>
+                    <IoCart size={35}/>
                     </Col>
                     <Col>
-                        <Button variant="success" className="buy-now-btn">Buy Now</Button>
+                        <Button variant="primary" className="buy-now-btn">Buy Now</Button>
                     </Col>
                 </Row>
             </Card.Body>
