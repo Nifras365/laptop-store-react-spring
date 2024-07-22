c9e6e1f5b2327d45f56f5c73d4c169d03987113d
Nifras365 <nifrasmjm56@outlook.com>
2024-07-22 21:20:09 +0530

    Update welcome.jsx

--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import NavbarBL from "../components/NavbarBL";
 import { useState,useEffect } from "react";
-import LaptopData from '../data/data.json'
+import LaptopData from '../data/data.json';
+import { Container,Col, Row } from "react-bootstrap";
 
 const Welcome = () => {
 
@@ -11,11 +12,18 @@ const Welcome = () => {
         setLaptop(LaptopData.laptop)
     })
 
-
     return(
     <div>
         <NavbarBL/>
         <h1>Laptop Store</h1>
+        <Container>
+            <Row>
+                <Col>
+                
+
+                </Col>
+            </Row>
+        </Container>
     </div>
     )
 }
