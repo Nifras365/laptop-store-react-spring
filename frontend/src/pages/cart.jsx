d90f3d049e90d035ca1c1dcae665a528cb3d3983
Nifras365 <nifrasmjm56@outlook.com>
2024-07-23 23:19:43 +0530

    Update cart.jsx

--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -3,12 +3,14 @@ import CartCard from "../components/CartCard";
 import { useLocation } from "react-router-dom";
 
 const Cart = () => {
-    
+    const location =  useLocation();
+    const{id, image, price, description} = location.state;
 
     return(
         <div>
             <h1>Under Construction</h1>
             <h2>Some laptop should be added here this is a cart...</h2>
+            <CartCard id={id} image={image} price={price} description={description}/>
         </div>
     );
 }
