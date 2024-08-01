cde249c895eb58a40212d32615aad2e8601c18b0
Nifras365 <nifrasmjm56@outlook.com>
2024-08-01 23:32:13 +0530

    sidebar-css

--- frontend/src/admin/components/Sidebar.jsx
+++ frontend/src/admin/components/Sidebar.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
+import '../css/SideBar.css';
 
 const SideBar = () => {
     return(
         <div>
-            <Sidebar>
-                <Menu>
-                    <SubMenu label="Laptops">
+            <Sidebar className="sidebar-main">
+                <Menu className="sidebar-menu">
+                    <SubMenu className="sidebar-submenu" label="Laptops">
                         <MenuItem>Add Laptops</MenuItem>
                         <MenuItem>Manage Laptops</MenuItem>
                     </SubMenu>
-                    <SubMenu label="Users">
-                        <MenuItem>Add Laptops</MenuItem>
+                    <SubMenu className="sidebar-submenu" label="Users">
+                        <MenuItem>View All Users</MenuItem>
                         <MenuItem>Manage Users</MenuItem>
                     </SubMenu>
                 </Menu>
