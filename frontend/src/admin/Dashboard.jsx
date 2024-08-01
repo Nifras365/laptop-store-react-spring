07455f17814a851d767d567f2ec17b9162d9155b
Nifras365 <nifrasmjm56@outlook.com>
2024-08-01 23:11:01 +0530

    pro-sidebar

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './css/Dashboard.css';
+import Sidebar from "./components/Sidebar";
 
 const Dashboard = () => {
     return(
         <div>
             Admin Features .....
+            <Sidebar/>
         <div>
             <button>Add Laptop</button>
             <button>See Users</button>
