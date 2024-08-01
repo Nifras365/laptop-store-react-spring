64ab53605b8aea77cb29a3415dd4056bcd535fde
Nifras365 <nifrasmjm56@outlook.com>
2024-08-01 23:39:40 +0530

    css

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -5,7 +5,6 @@ import SideBar from "./components/SideBar.jsx";
 const Dashboard = () => {
     return(
         <div>
-            <h1>Admin Features .....</h1>
             <SideBar/>
         </div>
     )
