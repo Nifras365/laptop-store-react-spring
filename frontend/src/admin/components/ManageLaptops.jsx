bec01e6efed9f4bc54e539a834a38ca7ea522551
Nifras365 <nifrasmjm56@outlook.com>
2024-08-02 21:01:20 +0530

    Create ManageLaptops.jsx

---
+++ frontend/src/admin/components/ManageLaptops.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManageLaptops = () => {
+    return(
+        <div>
+            This page is for manage laptops
+        </div>
+    );
+}
+
+export default ManageLaptops;
