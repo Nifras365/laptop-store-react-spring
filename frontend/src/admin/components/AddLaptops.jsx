d10e43986f2e55891e38163aa5b519842abc3862
Nifras365 <nifrasmjm56@outlook.com>
2024-08-02 18:31:58 +0530

    add-laptops-initial

---
+++ frontend/src/admin/components/AddLaptops.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddLaptops = () =>{
+    return(
+        <div>
+            This page is for add laptops
+        </div>
+    );
+}
+
+export default AddLaptops;
