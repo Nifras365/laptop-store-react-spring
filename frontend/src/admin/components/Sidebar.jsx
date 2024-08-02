769e95dfd18c0f6831c820767f59943ecdd775c0
Nifras365 <nifrasmjm56@outlook.com>
2024-08-03 01:04:31 +0530

    Update Sidebar.jsx

--- frontend/src/admin/components/Sidebar.jsx
+++ frontend/src/admin/components/Sidebar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import '../css/SideBar.css';
 
-const SideBar = () => {
+const SideBar = ({setSelectedComponent}) => {
     return (
         <div className="sidebar-container">
             <Sidebar className="sidebar-main">
                 <Menu className="sidebar-menu">
                     <SubMenu className="sidebar-submenu" label="Laptops">
-                        <MenuItem className="menu-item">Add Laptops</MenuItem>
+                        <MenuItem className="menu-item" >Add Laptops</MenuItem>
                         <MenuItem className="menu-item">Manage Laptops</MenuItem>
                     </SubMenu>
                     <SubMenu className="sidebar-submenu" label="Users">
