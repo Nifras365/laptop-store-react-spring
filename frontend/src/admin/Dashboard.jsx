f3e32951b4fee8446445954da2d53f2da19a5c31
Nifras365 <nifrasmjm56@outlook.com>
2024-08-03 01:24:51 +0530

    sidebar-click

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import './css/Dashboard.css';
 import SideBar from "./components/SideBar.jsx";
+import { useState } from "react";
+import AddLaptops from "./components/AddLaptops.jsx";
+import ManageLaptops from "./components/ManageLaptops.jsx";
 
 const Dashboard = () => {
+    
+    const[selectedComponent, setSelectedComponent] = useState('');
+
+    const selectComponent = () => {
+        switch(selectedComponent){
+            case 'add-laptops':
+                return <AddLaptops/>;
+            case 'manage-laptops':
+                return <ManageLaptops/>;
+            default:
+                return null;
+        }
+    }
+
+
     return(
-        <div>
-            <SideBar/>
+        <div className="dashboard-container">
+            <SideBar selectedComponent={selectedComponent} setSelectedComponent={setSelectedComponent}/>
+            <main>
+                {selectComponent()}
+            </main>
         </div>
     )
 }
