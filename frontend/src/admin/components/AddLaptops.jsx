b02b48737b672199bc8fd5cf48abdc4d70daea5c
Nifras365 <nifrasmjm56@outlook.com>
2024-08-03 16:24:35 +0530

    add-laptop-use-states

--- frontend/src/admin/components/AddLaptops.jsx
+++ frontend/src/admin/components/AddLaptops.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import '../css/AddLaptops.css';
 
 const AddLaptops = () =>{
+    const[price, setPrice] = useState('');
+    const[description, setDescription] = useState('');
+    const[image, setImage] = useState('');
+
     return(
         <div>
             This page is for add laptops
+            <form>
+
+            </form>
         </div>
     );
 }
