d0751b29e2d5d4b5d1a413ecbc7715090e7897a6
Nifras365 <nifrasmjm56@outlook.com>
2024-08-04 23:49:01 +0530

    Update ManageLaptops.jsx

--- frontend/src/admin/components/ManageLaptops.jsx
+++ frontend/src/admin/components/ManageLaptops.jsx
@@ -4,7 +4,7 @@ import '../css/ManageLaptops.css';
 const ManageLaptops = () => {
     return(
         <div>
-            This page is for manage laptops
+            This page is for manage laptops....
         </div>
     );
 }
