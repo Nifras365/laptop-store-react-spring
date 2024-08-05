66078769ecaed774151c4a364c6c81b8f3b81785
Nifras365 <nifrasmjm56@outlook.com>
2024-08-05 22:34:45 +0530

    Update LaptopCardAdmin.jsx

--- frontend/src/admin/components/LaptopCardAdmin.jsx
+++ frontend/src/admin/components/LaptopCardAdmin.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import '../css/LaptopCardAdmin.css';
+import { Card, Row, Col } from "react-bootstrap";
 
 const LaptopCardAdmin = () =>{
+    
+    const HandleEditClick = () => {
+
+    }
+    const HandleDeleteClick = () => {
+
+    }
+
     return(
-        <div>
-            Laptop Card For Admin
-        </div>
+        <Card className="laptop-card">
+            <Card.Img variant="top" src={image} className="laptop-card-img" />
+            <Card.Body>
+                <Card.Title>Price: ${price}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Row>
+                    <Col>
+                    <IoCart className="iocart" onClick={}/>
+                    </Col>
+                    <Col>
+                        <button className="buy-now-btn">Buy Now</button>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
     );
 }
 
 export default LaptopCardAdmin;
+
