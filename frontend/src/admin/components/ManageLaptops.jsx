723de9dbef20356bee41a7b2a0e096a669d773d3
Nifras365 <nifrasmjm56@outlook.com>
2024-08-05 22:46:40 +0530

    Update ManageLaptops.jsx

--- frontend/src/admin/components/ManageLaptops.jsx
+++ frontend/src/admin/components/ManageLaptops.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import '../css/ManageLaptops.css';
+import LaptopCardAdmin from "./LaptopCardAdmin";
+import LaptopData from '../data/data.json';
+import { useState, useEffect } from "react";
 
 const ManageLaptops = () => {
+
+    const[laptop, setlaptop] = useState([]);
+
+    useEffect(() => {
+        setlaptop(LaptopData.laptop)
+    },[])
+
     return(
         <div>
             This page is for manage laptops....
