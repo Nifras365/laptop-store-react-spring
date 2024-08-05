4795aa64f548768a7213923928ea40d6cc445a7e
Nifras365 <nifrasmjm56@outlook.com>
2024-08-05 22:01:23 +0530

    //

--- olsbackend/src/main/java/laptop_store/olsbackend/entity/LaptopEntity.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/entity/LaptopEntity.java
@@ -3,5 +3,5 @@ package laptop_store.olsbackend.entity;
 public class LaptopEntity {
     private Long id;
 
-    // will add other entities later after finalizing the structure 
+    // will add other entities later after finalizing the structure
 }
