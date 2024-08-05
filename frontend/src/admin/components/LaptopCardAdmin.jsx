9dcee26fe56a9602bae6b00bcb5c243e92c5169c
Nifras365 <nifrasmjm56@outlook.com>
2024-08-05 22:09:41 +0530

    laptop-card-admin

---
+++ frontend/src/admin/components/LaptopCardAdmin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LaptopCardAdmin = () =>{
+    return(
+        <div>
+            Laptop Card For Admin
+        </div>
+    );
+}
+
+export default LaptopCardAdmin;
