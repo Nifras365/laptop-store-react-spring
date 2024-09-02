1d9e26b741d8ff93aa793631c73961277684ba9d
Nifras365 <nifrasmjm56@outlook.com>
2024-09-03 03:02:43 +0530

    createUsers - Controller

--- olsbackend/src/main/java/laptop_store/olsbackend/controller/UsersController.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/controller/UsersController.java
@@ -1,9 +1,20 @@
 package laptop_store.olsbackend.controller;
 
+import laptop_store.olsbackend.dto.UsersDTO;
+import laptop_store.olsbackend.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+    @Autowired
+    private UsersService usersService;
+    @PostMapping("/create")
+    public UsersDTO createUsers(@RequestBody UsersDTO usersDTO){
+        return usersService.createUser(usersDTO);
+    }
 }
