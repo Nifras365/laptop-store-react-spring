adc872ac15bf193263bf5653f1f7aea3208fb47a
Nifras365 <nifrasmjm56@outlook.com>
2024-09-03 00:36:57 +0530

    Update UsersServiceImpl.java

--- olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
@@ -1,7 +1,14 @@
 package laptop_store.olsbackend.service;
 
+import laptop_store.olsbackend.mapper.UsersMapper;
+import laptop_store.olsbackend.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersServiceImpl implements UsersService{
+    @Autowired
+    private UsersRepository usersRepository;
+    @Autowired
+    private UsersMapper usersMapper;
 }
