90a9b72928dc2a21184862d3f0e715cf1462db49
Nifras365 <nifrasmjm56@outlook.com>
2024-09-02 23:27:48 +0530

    UsersEntity

---
+++ olsbackend/src/main/java/laptop_store/olsbackend/entity/UsersEntity.java
@@ -0,0 +1,12 @@
+package laptop_store.olsbackend.entity;
+
+public class UsersEntity {
+    private Long id;
+    private String email;
+    private String name;
+    private String password;
+    private String confirmPassword;
+    private Long phone;
+    private String address;
+    private String country;
+}
