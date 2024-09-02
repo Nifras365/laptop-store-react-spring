8086b5f6cf5dc7eb7032f0700ce7d7406a80cb98
Nifras365 <nifrasmjm56@outlook.com>
2024-09-02 23:53:16 +0530

    usersDTO

---
+++ olsbackend/src/main/java/laptop_store/olsbackend/dto/UsersDTO.java
@@ -0,0 +1,11 @@
+package laptop_store.olsbackend.dto;
+
+public class UsersDTO {
+    private String email;
+    private String name;
+    private String password;
+    private String confirmPassword;
+    private Long phone;
+    private String address;
+    private String country;
+}
