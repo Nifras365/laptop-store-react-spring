fcdc3bee0fb810de3411885bec6ff33506723876
Nifras365 <nifrasmjm56@outlook.com>
2024-09-02 23:56:09 +0530

    user - mapper

--- olsbackend/src/main/java/laptop_store/olsbackend/mapper/UsersMapper.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/mapper/UsersMapper.java
@@ -1,8 +1,11 @@
 package laptop_store.olsbackend.mapper;
 
+import laptop_store.olsbackend.dto.UsersDTO;
+import laptop_store.olsbackend.entity.UsersEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UsersMapper {
-
+    UsersDTO mapToDto(UsersEntity usersEntity);
+    UsersEntity mapToEntity(UsersDTO usersDTO);
 }
