c4bd4f6172d796dae7e5a72dec7634c6a981b8bd
Nifras365 <nifrasmjm56@outlook.com>
2024-09-04 02:47:27 +0530

    create - user

--- olsbackend/src/main/java/laptop_store/olsbackend/OlsbackendApplication.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/OlsbackendApplication.java
@@ -3,9 +3,8 @@ package laptop_store.olsbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller","service","entity", "repository", "dto", "mapper","exceptions"})
+@SpringBootApplication
 public class OlsbackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OlsbackendApplication.class, args);
 	}
