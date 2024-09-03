cf18cd47b1ba4a8013e97a787a8b09264412a7a2
Nifras365 <nifrasmjm56@outlook.com>
2024-09-03 14:37:31 +0530

    existingUsers - check

--- olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
@@ -2,10 +2,13 @@ package laptop_store.olsbackend.service;
 
 import laptop_store.olsbackend.dto.UsersDTO;
 import laptop_store.olsbackend.entity.UsersEntity;
+import laptop_store.olsbackend.exceptions.ItemAlreadyExistsException;
 import laptop_store.olsbackend.mapper.UsersMapper;
 import laptop_store.olsbackend.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsersServiceImpl implements UsersService{
@@ -15,6 +18,13 @@ public class UsersServiceImpl implements UsersService{
     private UsersMapper usersMapper;
 
     public UsersDTO createUser(UsersDTO usersDTO){
+
+        Optional<UsersEntity> existingUsers = usersRepository.findByEmail(usersDTO.getEmail());
+
+        if (existingUsers.isPresent()){
+            throw new ItemAlreadyExistsException("User with given email already exists : " + usersDTO.getEmail());
+        }
+
         UsersEntity usersEntity = new UsersEntity();
 
         usersEntity.setEmail(usersDTO.getEmail());
