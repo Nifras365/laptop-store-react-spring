75fb447fb6a188434905cd3ff983ebba0b6935a1
Nifras365 <nifrasmjm56@outlook.com>
2024-09-04 02:17:14 +0530

    ResponseDTO

--- olsbackend/src/main/java/laptop_store/olsbackend/dto/ResponseDTO.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/dto/ResponseDTO.java
@@ -1,12 +1,20 @@
 package laptop_store.olsbackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class ResponseDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO<T>{
 
     private Integer status;
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
     public ResponseDTO(Integer status, String message){
         this.status = status;
         this.message = message;
