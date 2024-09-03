42c145f0532a70fe494cc6ef71724c3b7c3deb40
Nifras365 <nifrasmjm56@outlook.com>
2024-09-03 14:35:12 +0530

    findByEmail

--- olsbackend/src/main/java/laptop_store/olsbackend/repository/UsersRepository.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/repository/UsersRepository.java
@@ -4,6 +4,9 @@ import laptop_store.olsbackend.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
+    Optional<UsersEntity> findByEmail(String email);
 }
