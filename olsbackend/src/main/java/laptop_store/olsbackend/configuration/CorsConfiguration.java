671689957ff8b1c8b20fb69954be9cba3d7e48e1
Nifras365 <nifrasmjm56@outlook.com>
2024-09-05 02:11:17 +0530

    CorsConfiguration

--- olsbackend/src/main/java/laptop_store/olsbackend/configuration/CorsConfiguration.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/configuration/CorsConfiguration.java
@@ -1,4 +1,16 @@
 package laptop_store.olsbackend.configuration;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    public void myCorsMapping(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
