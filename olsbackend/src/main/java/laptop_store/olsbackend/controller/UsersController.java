fa114f931494b4d3a54b5f134c35fa0b35d0063e
Nifras365 <nifrasmjm56@outlook.com>
2024-09-04 18:04:16 +0530

    Update UsersController.java

--- olsbackend/src/main/java/laptop_store/olsbackend/controller/UsersController.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/controller/UsersController.java
@@ -1,5 +1,6 @@
 package laptop_store.olsbackend.controller;
 
+import laptop_store.olsbackend.dto.LoginDTO;
 import laptop_store.olsbackend.dto.ResponseDTO;
 import laptop_store.olsbackend.dto.UsersDTO;
 import laptop_store.olsbackend.service.UsersService;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/users")
 public class UsersController {
@@ -20,5 +23,29 @@ public class UsersController {
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO<Long>> createUsers(@RequestBody UsersDTO usersDTO){
         return ResponseEntity.ok().body(new ResponseDTO<>(HttpStatus.OK.value(), "User Created Successfully !!", usersService.createUser(usersDTO)));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO<Long>> userLogin(@RequestBody LoginDTO loginDTO){
+        String email = loginDTO.getEmail();
+        String password = loginDTO.getPassword();
+
+        try {
+            Optional<UsersDTO> existingUserCheck = usersService.findByEmail(email);
+            if (existingUserCheck.isPresent()){
+                if (existingUserCheck.get().getPassword().equals(password)){
+                    return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK.value(), "Login Successful !!");
+                }
+                else {
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(HttpStatus.UNAUTHORIZED.value(), "Wrong Password !");
+                }
+            }
+            else {
+               return ResponseEntity.status(HttpStatus.NOT_FOUND).body(HttpStatus.NOT_FOUND, "User Not found !");
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error !");
+        }
     }
 }
