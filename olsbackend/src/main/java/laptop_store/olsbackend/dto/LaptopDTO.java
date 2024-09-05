26dd4801218ae952bd747530a200cb0b17d6fb4f
Nifras365 <nifrasmjm56@outlook.com>
2024-09-06 02:55:57 +0530

    LaptopDTO

--- olsbackend/src/main/java/laptop_store/olsbackend/dto/LaptopDTO.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/dto/LaptopDTO.java
@@ -1,4 +1,20 @@
 package laptop_store.olsbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LaptopDTO {
+    private String email;
+    private String name;
+    private String password;
+    private String confirmPassword;
+    private Long phone;
+    private String address;
+    private String country;
 }
