a58f7d6a0dc41ca005c3a040bfc77f54a521c7ff
Nifras365 <nifrasmjm56@outlook.com>
2024-09-06 03:02:07 +0530

    handleUnauthorizedException

--- olsbackend/src/main/java/laptop_store/olsbackend/exceptions/CustomExceptionHandler.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/exceptions/CustomExceptionHandler.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
     @ExceptionHandler(ItemAlreadyExistsException.class)
     public ResponseEntity<ResponseDTO> handleItemAlreadyExistsException(ItemAlreadyExistsException exception){
-        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.CONTINUE.value(), exception.getMessage());
+        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.CONFLICT.value(), exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(responseDTO);
+    }
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ResponseDTO> handleUnauthorizedException(UnauthorizedException e){
+        ResponseDTO responseDTO1 = new ResponseDTO<>(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseDTO1);
     }
 }
--- olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/UsersServiceImpl.java
@@ -3,6 +3,7 @@ package laptop_store.olsbackend.service;
 import laptop_store.olsbackend.dto.UsersDTO;
 import laptop_store.olsbackend.entity.UsersEntity;
 import laptop_store.olsbackend.exceptions.ItemAlreadyExistsException;
+import laptop_store.olsbackend.exceptions.UnauthorizedException;
 import laptop_store.olsbackend.mapper.UsersMapper;
 import laptop_store.olsbackend.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class UsersServiceImpl implements UsersService{
             throw new ItemAlreadyExistsException("User with given email already exists : " + usersDTO.getEmail());
         }
         if (!usersDTO.getPassword().equals(usersDTO.getConfirmPassword())){
-            throw new ItemAlreadyExistsException("Password didn't match !!!: " + usersDTO.getPassword());
+            throw new UnauthorizedException("Password didn't match !!!: " + usersDTO.getPassword());
         }
         return usersRepository.save(UsersEntity.builder()
                 .email(usersDTO.getEmail())
