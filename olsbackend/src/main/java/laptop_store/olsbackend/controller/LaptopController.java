2aa8ee3d3898f43f0d71ba50bd211384f24f1bf9
Nifras365 <nifrasmjm56@outlook.com>
2024-09-06 19:51:58 +0530

    createLaptops - controller

--- olsbackend/src/main/java/laptop_store/olsbackend/controller/LaptopController.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/controller/LaptopController.java
@@ -1,4 +1,21 @@
 package laptop_store.olsbackend.controller;
 
+import laptop_store.olsbackend.dto.LaptopDTO;
+import laptop_store.olsbackend.dto.ResponseDTO;
+import laptop_store.olsbackend.service.LaptopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/laptops")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LaptopController {
+    @Autowired
+    private LaptopService laptopService;
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO<Long>> createLaptops(@RequestBody LaptopDTO laptopDTO){
+        return ResponseEntity.ok().body(new ResponseDTO<>(HttpStatus.OK.value(), "Laptop added successfully !!!", laptopService.createLaptop(laptopDTO)));
+    }
 }
