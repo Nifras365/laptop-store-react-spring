47bafab29d629a2884a420467f9a85dd1a10e268
Nifras365 <nifrasmjm56@outlook.com>
2024-09-06 19:35:44 +0530

    LaptopServiceImpl

--- olsbackend/src/main/java/laptop_store/olsbackend/service/LaptopServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/LaptopServiceImpl.java
@@ -1,4 +1,15 @@
 package laptop_store.olsbackend.service;
 
-public class LaptopServiceImpl {
+import laptop_store.olsbackend.mapper.LaptopMapper;
+import laptop_store.olsbackend.repository.LaptopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LaptopServiceImpl implements LaptopService{
+    @Autowired
+    private LaptopRepository laptopRepository;
+    @Autowired
+    private LaptopMapper laptopMapper;
+
 }
