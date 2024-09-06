2e8f64f380e00f9e1f03c3395bd7fbabb89145ba
Nifras365 <nifrasmjm56@outlook.com>
2024-09-06 19:31:52 +0530

    LaptopMapper

--- olsbackend/src/main/java/laptop_store/olsbackend/mapper/LaptopMapper.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/mapper/LaptopMapper.java
@@ -1,4 +1,11 @@
 package laptop_store.olsbackend.mapper;
 
+import laptop_store.olsbackend.dto.LaptopDTO;
+import laptop_store.olsbackend.entity.LaptopEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface LaptopMapper {
+    LaptopDTO mapToDto(LaptopEntity laptopEntity);
+    LaptopEntity mapToEntity(LaptopDTO laptopDTO);
 }
