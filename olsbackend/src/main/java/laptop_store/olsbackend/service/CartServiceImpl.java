9a53328953b129b7c5b4a2805a1a6e85017b1722
Nifras365 <nifrasmjm56@outlook.com>
2024-09-12 20:39:18 +0530

    laptopAvailability - check

--- olsbackend/src/main/java/laptop_store/olsbackend/service/CartServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/CartServiceImpl.java
@@ -2,22 +2,38 @@ package laptop_store.olsbackend.service;
 
 import laptop_store.olsbackend.dto.CartDTO;
 import laptop_store.olsbackend.entity.CartEntity;
+import laptop_store.olsbackend.entity.LaptopEntity;
+import laptop_store.olsbackend.exceptions.ItemNotFoundException;
 import laptop_store.olsbackend.mapper.CartMapper;
 import laptop_store.olsbackend.repository.CartRepository;
+import laptop_store.olsbackend.repository.LaptopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements CartService{
     @Autowired
     private CartRepository cartRepository;
+
+    @Autowired
+    private LaptopRepository laptopRepository;
     @Autowired
     private CartMapper cartMapper;
     @Override
     public Long addToCart(CartDTO cartDTO){
-        return cartRepository.save(CartEntity.builder()
-                .laptopID(cartDTO.getLaptopID())
-                .quantity(cartDTO.getQuantity())
-                .totalPrice(cartDTO.getTotalPrice()).build()).getCartID();
+
+        Optional<LaptopEntity> laptopAvailability = laptopRepository.findById(cartDTO.getLaptopID());
+
+        if (laptopAvailability.isPresent()){
+            return cartRepository.save(CartEntity.builder()
+                    .laptopID(cartDTO.getLaptopID())
+                    .quantity(cartDTO.getQuantity())
+                    .totalPrice(cartDTO.getTotalPrice()).build()).getCartID();
+        }
+        else {
+            throw new ItemNotFoundException("Laptop Doesn't exist with given laptop id ");
+        }
     }
 }
