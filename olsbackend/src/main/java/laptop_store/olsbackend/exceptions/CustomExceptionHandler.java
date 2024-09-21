9223ddab682d62acc35ebbe28d9507998cc49ab3
Nifras365 <nifrasmjm56@outlook.com>
2024-09-21 19:26:48 +0530

    adding a type - void

--- olsbackend/src/main/java/laptop_store/olsbackend/exceptions/CustomExceptionHandler.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/exceptions/CustomExceptionHandler.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(ItemAlreadyExistsException.class)
-    public ResponseEntity<ResponseDTO> handleItemAlreadyExistsException(ItemAlreadyExistsException exception){
-        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.CONFLICT.value(), exception.getMessage());
+    public ResponseEntity<ResponseDTO<Void>> handleItemAlreadyExistsException(ItemAlreadyExistsException exception){
+        ResponseDTO<Void> responseDTO = new ResponseDTO<>(HttpStatus.CONFLICT.value(), exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(responseDTO);
     }
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ResponseDTO> handleUnauthorizedException(UnauthorizedException e){
-        ResponseDTO responseDTO1 = new ResponseDTO(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+    public ResponseEntity<ResponseDTO<Void>> handleUnauthorizedException(UnauthorizedException e){
+        ResponseDTO<Void> responseDTO1 = new ResponseDTO<>(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseDTO1);
     }
     @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ResponseDTO> handleItemNotFoundException(ItemNotFoundException ie){
-        ResponseDTO responseDTO2 = new ResponseDTO(HttpStatus.NOT_FOUND.value(), ie.getMessage());
+    public ResponseEntity<ResponseDTO<Void>> handleItemNotFoundException(ItemNotFoundException ie){
+        ResponseDTO<Void> responseDTO2 = new ResponseDTO<>(HttpStatus.NOT_FOUND.value(), ie.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO2);
     }
     @ExceptionHandler(OutOfRangeException.class)
-    public ResponseEntity<ResponseDTO> handleOutOfRangeException(OutOfRangeException oe){
-        ResponseDTO responseDTO3 = new ResponseDTO(HttpStatus.BAD_REQUEST.value(), oe.getMessage());
+    public ResponseEntity<ResponseDTO<Void>> handleOutOfRangeException(OutOfRangeException oe){
+        ResponseDTO<Void> responseDTO3 = new ResponseDTO<>(HttpStatus.BAD_REQUEST.value(), oe.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO3);
     }
 }
