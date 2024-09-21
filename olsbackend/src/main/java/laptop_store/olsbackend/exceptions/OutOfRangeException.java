11c131c451ac55b737f3e4e69d1710435ef04533
Nifras365 <nifrasmjm56@outlook.com>
2024-09-21 19:08:02 +0530

    OutOfRangeException

---
+++ olsbackend/src/main/java/laptop_store/olsbackend/exceptions/OutOfRangeException.java
@@ -0,0 +1,7 @@
+package laptop_store.olsbackend.exceptions;
+
+public class OutOfRangeException extends RuntimeException{
+    public OutOfRangeException(String message){
+        super(message);
+    }
+}
