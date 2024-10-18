c0e0952997c0f3cb96b3a4e5ea01d66938c60629
Nifras365 <nifrasmjm56@outlook.com>
2024-10-18 23:48:54 +0530

    createOrder

--- olsbackend/src/main/java/laptop_store/olsbackend/service/OrdersServiceImpl.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/OrdersServiceImpl.java
@@ -1,6 +1,7 @@
 package laptop_store.olsbackend.service;
 
 import laptop_store.olsbackend.dto.OrderDTO;
+import laptop_store.olsbackend.entity.OrdersEntity;
 import laptop_store.olsbackend.mapper.OrdersMapper;
 import laptop_store.olsbackend.repository.OrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class OrdersServiceImpl implements OrdersService{
     private OrdersMapper ordersMapper;
 
     public Long createOrder(OrderDTO orderDTO){
-        
+        return ordersRepository.save(OrdersEntity.builder()
+                .UserID(orderDTO.getUserID())
+                .orderItems(orderDTO.getOrderItems())
+                .FinalPrice(orderDTO.getFinalPrice()).build()).getOrderId();
     }
 }
