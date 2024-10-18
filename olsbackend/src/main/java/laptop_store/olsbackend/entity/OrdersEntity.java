94043ed45befb4d41fbfb71ef2d362110197a2fb
Nifras365 <nifrasmjm56@outlook.com>
2024-10-18 23:14:07 +0530

    Update OrdersEntity.java

--- olsbackend/src/main/java/laptop_store/olsbackend/entity/OrdersEntity.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/entity/OrdersEntity.java
@@ -1,7 +1,16 @@
 package laptop_store.olsbackend.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Map;
+
 public class OrdersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long OrderId;
     private Long UserID;
+    private Map<String, Integer> orderItems;
     private Long FinalPrice;
 }
