e77f0d51d369d00829e439ddfb83ec5477e40d56
Nifras365 <nifrasmjm56@outlook.com>
2024-10-19 20:08:10 +0530

    Update OrdersController.java

--- olsbackend/src/main/java/laptop_store/olsbackend/controller/OrdersController.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/controller/OrdersController.java
@@ -1,11 +1,22 @@
 package laptop_store.olsbackend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import laptop_store.olsbackend.dto.OrderDTO;
+import laptop_store.olsbackend.dto.ResponseDTO;
+import laptop_store.olsbackend.service.OrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
 @CrossOrigin(origins = "http://localhost:3000")
 public class OrdersController {
+    @Autowired
+    private OrdersService ordersService;
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO<Long>> createOrder(@RequestBody OrderDTO orderDTO){
+        return ResponseEntity.ok().body(new ResponseDTO<>(HttpStatus.OK.value(),
+                "Order Placed Successfully!!!"));
+    }
 }
