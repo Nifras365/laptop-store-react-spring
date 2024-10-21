df56459a4fc57a69763d5b186004535f6dc20463
Nifras365 <nifrasmjm56@outlook.com>
2024-10-22 02:25:11 +0530

    Update OrdersMapper.java

--- olsbackend/src/main/java/laptop_store/olsbackend/mapper/OrdersMapper.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/mapper/OrdersMapper.java
@@ -1,11 +1,26 @@
 package laptop_store.olsbackend.mapper;
 
 import laptop_store.olsbackend.dto.OrderDTO;
+import laptop_store.olsbackend.dto.OrderItemDTO;
+import laptop_store.olsbackend.entity.OrderItemEntity;
 import laptop_store.olsbackend.entity.OrdersEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.TargetType;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrdersMapper {
+
+    @Mapping(target = "orderItemDTOS", source = "orderItemEntities")
     OrderDTO mapToDto(OrdersEntity ordersEntity);
+    @Mapping(target = "orderItemEntities", source = "orderItemDTOS")
     OrdersEntity mapToEntity(OrderDTO orderDTO);
+
+    OrderItemDTO mapToDto(OrderItemEntity orderItemEntity);
+    OrderItemEntity mapToEntity(OrderItemDTO orderItemDTO);
+
+    List<OrderItemDTO> mapOrderItemToDto(List<OrderItemEntity> orderItemEntities);
+    List<OrderItemEntity> mapOrderItemToEntity(List<OrderItemDTO> orderItemDTOS);
 }
