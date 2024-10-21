8907f316b05ed636b94e30dcdd4d9beca004db1c
Nifras365 <nifrasmjm56@outlook.com>
2024-10-22 02:38:49 +0530

    getAllOrders

--- olsbackend/src/main/java/laptop_store/olsbackend/service/OrdersService.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/service/OrdersService.java
@@ -2,6 +2,9 @@ package laptop_store.olsbackend.service;
 
 import laptop_store.olsbackend.dto.OrderDTO;
 
+import java.util.List;
+
 public interface OrdersService {
     Long createOrder(OrderDTO orderDTO);
+    List<OrderDTO> getAllOrders();
 }
