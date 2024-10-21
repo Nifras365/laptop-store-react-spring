033622d911e9fb82577208a939996966674bb0c8
Nifras365 <nifrasmjm56@outlook.com>
2024-10-21 14:37:48 +0530

    Update OrderItemEntity.java

--- olsbackend/src/main/java/laptop_store/olsbackend/entity/OrderItemEntity.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/entity/OrderItemEntity.java
@@ -1,6 +1,13 @@
 package laptop_store.olsbackend.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
 public class OrderItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderItemId;
     private String title;
     private String quantity;
