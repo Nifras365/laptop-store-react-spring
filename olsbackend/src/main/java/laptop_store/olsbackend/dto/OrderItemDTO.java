52e1d7e9d7f13adea7b634b3f7798f9e309a7629
Nifras365 <nifrasmjm56@outlook.com>
2024-10-21 14:42:44 +0530

    Create OrderItemDTO.java

---
+++ olsbackend/src/main/java/laptop_store/olsbackend/dto/OrderItemDTO.java
@@ -0,0 +1,15 @@
+package laptop_store.olsbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItemDTO {
+    private String title;
+    private Long quantity;
+}
