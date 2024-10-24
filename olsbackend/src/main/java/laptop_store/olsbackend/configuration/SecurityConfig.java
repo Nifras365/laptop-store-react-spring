d722f811efbe04e20a04e8c8837c0329663ef6d7
Nifras365 <nifrasmjm56@outlook.com>
2024-10-25 01:09:49 +0530

    security-config

--- olsbackend/src/main/java/laptop_store/olsbackend/configuration/SecurityConfig.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/configuration/SecurityConfig.java
@@ -1,4 +1,14 @@
 package laptop_store.olsbackend.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return ;
+    }
 }
