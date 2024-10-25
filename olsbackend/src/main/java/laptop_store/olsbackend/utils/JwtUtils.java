193aa07cb7f783741f60b65a772b9b565c87e96a
Nifras365 <nifrasmjm56@outlook.com>
2024-10-25 21:22:19 +0530

    JwtUtils - generateToken

--- olsbackend/src/main/java/laptop_store/olsbackend/utils/JwtUtils.java
+++ olsbackend/src/main/java/laptop_store/olsbackend/utils/JwtUtils.java
@@ -1,4 +1,25 @@
 package laptop_store.olsbackend.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+@Component
 public class JwtUtils {
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+    private String jwtSecret;
+
+    public String generateToken(String email, Map<String, Object> claims){
+        return Jwts.builder()
+                .claims(claims)
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
