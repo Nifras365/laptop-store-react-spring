581f123e8079513f25a39a4fbe86a80e08127228
Nifras365 <nifrasmjm56@outlook.com>
2024-11-02 13:09:45 +0530

    ProtectedAdminRoute

---
+++ frontend/src/routes/ProtectedAdminRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedAdminRoute = ({children}) => {
+    const role = localStorage.getItem('userRole');
+    const token = localStorage.getItem('token');
+
+    if(role === 'ADMIN' && token){
+        return children;
+    }
+    else{
+        return <Navigate to="/login"/>
+    }
+};
+
+export default ProtectedAdminRoute;
