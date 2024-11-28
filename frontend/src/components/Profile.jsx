e6aea81696abc3c6da964830199e594908eb6950
Nifras365 <nifrasmjm56@outlook.com>
2024-11-28 20:30:41 +0530

    getUserDetails

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,6 +1,24 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
 
 const Profile = () => {
+
+    useEffect(()=>{
+        const userID = localStorage.getItem('userID');
+
+        async function getUserDetails() {
+            try {
+                const response = await axios.get(`http://localhost:8080/users/userdetails/${userID}`);
+
+                console.log("Here is the data: ",response.data.data[0]);
+            } catch (error) {
+                console.error("Error fetching details", error);
+            }
+        }
+        getUserDetails();
+    },[])
+
     return(
         <div>
             Hi User
