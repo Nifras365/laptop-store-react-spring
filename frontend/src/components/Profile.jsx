63a571fa9540324a56ba012f8028bf521a6c2bb1
Nifras365 <nifrasmjm56@outlook.com>
2024-11-30 00:04:40 +0530

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 const Profile = () => {
 
+    const[userDetails, setUserDetails] = useState(null);
+
     useEffect(()=>{
         const userID = localStorage.getItem('userID');
 
@@ -12,6 +14,10 @@ const Profile = () => {
                 const response = await axios.get(`http://localhost:8080/users/userdetails/${userID}`);
 
                 console.log("Here is the data: ",response.data.data[0]);
+
+                const userData = response.data.data[0];
+                setUserDetails(userData);
+
             } catch (error) {
                 console.error("Error fetching details", error);
             }
