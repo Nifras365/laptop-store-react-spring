2b35921d05ebbb2442e106029b7b55f86ae019e0
Nifras365 <nifrasmjm56@outlook.com>
2024-11-30 22:27:37 +0530

    my profile css design

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { useEffect, useState } from "react";
+import './css/Profile.css';
 
 const Profile = () => {
 
@@ -25,37 +26,37 @@ const Profile = () => {
         getUserDetails();
     },[])
 
-    return(
-        <div>
-            {userDetails ? (
-                <div>
-                  <h1>Hi, {userDetails.name}</h1>
-                    <div>
-                        <label>Name:</label>
-                        <input value={userDetails.name}></input>
-                    </div>
-                    <div>
-                        <label>Email:</label>
-                        <input value={userDetails.email}></input>
-                    </div>
-                    <div>
-                        <label>Country:</label>
-                        <input value={userDetails.country}></input>
-                    </div>
-                    <div>
-                        <label>Address:</label>
-                        <input value={userDetails.address}></input>
-                    </div>
-                    <div>
-                        <label>Phone:</label>
-                        <input value={userDetails.phone}></input>
-                    </div>
+    return (
+        <div className="profile-container">
+          {userDetails ? (
+            <div className="profile-card">
+              <h1>Hi, {userDetails.name}</h1>
+                <div className="profile-field">
+                    <label>Name:</label>
+                    <input value={userDetails.name} readOnly />
                 </div>
-            ) : (
-                <p>Loading user details...</p>
-            )}
+                <div className="profile-field">
+                    <label>Email:</label>
+                    <input value={userDetails.email} readOnly />
+                </div>
+                <div className="profile-field">
+                    <label>Country:</label>
+                    <input value={userDetails.country} readOnly />
+                </div>
+                <div className="profile-field">
+                    <label>Address:</label>
+                    <input value={userDetails.address} readOnly />
+                </div>
+                <div className="profile-field">
+                    <label>Phone:</label>
+                    <input value={userDetails.phone} readOnly />
+                </div>
+            </div>
+          ) : (
+            <p>Loading user details...</p>
+          )}
         </div>
-    );
+      );
 }
 
 export default Profile;
