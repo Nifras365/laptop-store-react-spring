c5515d66943cca25750e030b11fba9690a1933e9
Nifras365 <nifrasmjm56@outlook.com>
2025-08-24 21:31:29 +0530

    NavbarBL - UI updated

--- frontend/src/components/NavbarBL.jsx
+++ frontend/src/components/NavbarBL.jsx
@@ -1,39 +1,53 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Container, NavbarBrand,Form, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container, Form, FormControl, InputGroup, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { IoPersonOutline } from "react-icons/io5";
-import { IoSearch } from "react-icons/io5";
+import { IoPersonOutline, IoSearch } from "react-icons/io5";
 import './css/NavbarBL.css';
 
 const NavbarBL = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar variant="light" expand="lg" className="custom-navbar shadow-sm" sticky="top">
       <Container>
-        <NavbarBrand href="#home">MyLapStore</NavbarBrand>
+        <Navbar.Brand href="/" className="navbar-brand-custom">MyLapStore</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="mx-auto">
+            <Nav.Link href="/" className="nav-link-custom">Home</Nav.Link>
+            <Nav.Link href="/" className="nav-link-custom">Laptops</Nav.Link>
+            <Nav.Link href="/" className="nav-link-custom">Accessories</Nav.Link>
           </Nav>
-          <Form className="d-flex mx-auto">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="form-navbar"
-              aria-label="Search"
-            />
-            <IoSearch size={40}/>
-          </Form>
-          <Nav className="ms-auto">
-            <NavDropdown className='dropdown-title' title={<IoPersonOutline size={25}/>} id="basic-nav-dropdown">
-              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-              <NavDropdown.Item href="/cart">Cart</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Contact Us</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+
+          <div className="d-flex align-items-center">
+            <Form className="d-flex search-form">
+              <InputGroup>
+                <FormControl
+                  type="search"
+                  placeholder="Search products..."
+                  className="search-input"
+                  aria-label="Search"
+                />
+                <Button variant="outline-secondary" type="submit" className="search-button">
+                  <IoSearch />
+                </Button>
+              </InputGroup>
+            </Form>
+
+            <Nav>
+              <NavDropdown 
+                className='profile-dropdown' 
+                title={<IoPersonOutline size={25} />} 
+                id="basic-nav-dropdown"
+                align="end" 
+              >
+                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+                <NavDropdown.Item href="/cart">My Cart</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Contact Us</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </div>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
